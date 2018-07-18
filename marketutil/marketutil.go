ce3e3604897b1b1c300263dc69db15649998866e
指尖下的幽灵 <913337456@qq.com>
2018-07-18 15:59:27 +0800

    change approve type

--- marketutil/marketutil.go
+++ marketutil/marketutil.go
@@ -115,6 +115,7 @@ func (t *token) convert() types.Token {
 		dst.IcoPrice.SetString(t.IcoPrice)
 	}
 	dst.AreaType = t.AreaType
+	dst.ApproveType = t.ApproveType
 	return dst
 }
 
@@ -145,7 +146,10 @@ func getTokenAndMarketFromDB(tokenfile string) (
 	if err := json.Unmarshal(bs, &list); err != nil {
 		log.Fatalf("market util unmarshal tokens failed:%s", err.Error())
 	}
-
+	data2,_ := json.Marshal(types.Token{})
+	fmt.Println(string(data2))
+	data,_ := json.Marshal(list)
+	panic(string(data))
 	for _, v := range list {
 		if v.Deny == false {
 			t := v.convert()
